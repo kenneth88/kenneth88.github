75716625746d00bd6f406bac35ab25878c5d9311
kenneth peralta <kenneth.peralta@hotmail.com>
2016-03-24 13:39:57 -0700

    finished video # 9 added our app and added a user

--- public_html/js/blog.js
+++ public_html/js/blog.js
@@ -1,7 +1,15 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$(function () {
+    var APPLICATION_ID = "01C45C3C-1A36-B211-FFCB-8ECE4D2EC100",
+    SECRET_KEY = "B264B60B-124A-6834-FFF6-6240D396AE00",
+    VERSION = "v1";
+    
+    Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+    
+    var user = new Backendless.user();
+    user.email = "kenneth.peralta@hotmail.com";
+    user.password = "sfgiantsRule8";
+    Backendless.userService.register(user);
+    
+});
 
 
