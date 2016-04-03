9f7b083e0a884660e472d7b93f145e72c3deec5a
kenneth peralta <kenneth.peralta@hotmail.com>
2016-04-03 15:46:53 -0700

    finished  video # 19 rendering login template

---
+++ public_html/js/admin.js
@@ -0,0 +1,22 @@
+$(function () {
+    var APPLICATION_ID = "01C45C3C-1A36-B211-FFCB-8ECE4D2EC100",
+    SECRET_KEY = "B264B60B-124A-6834-FFF6-6240D396AE00",
+    VERSION = "v1";
+    
+    Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+    
+    var loginScript = $("#login-template").html();
+    var loginTemplate = Handlebars.compile(loginScript);
+    
+    $('.main-container').html(loginTemplate);
+});
+
+function Posts(args){
+    args = args || {};
+    this.title = args.title || "";
+    this.content = args.content || "";
+    this.authorEmail = args.authorEmail || "";
+}
+
+
+
